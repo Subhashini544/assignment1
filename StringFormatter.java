e477fb473405b6727c7d3f4110e04c6a68b2395c
subhashini544 <subhashini.j2000@gmail.com>
2022-01-07 09:32:31 +0530

    sorting java programs

---
+++ StringFormatter.java
@@ -0,0 +1,15 @@
+package com.te.assignment;
+
+	public class StringFormatter {  
+		public static String reverseString(String str){  
+		    char ch[]=str.toCharArray();  
+		    String rev="";  
+		    for(int i=ch.length-1;i>=0;i--){  
+		        rev+=ch[i];  
+		    }  
+		    return rev;  
+		}  
+		}  
+
+
+
---
+++ TestStringFormatter.java
@@ -0,0 +1,8 @@
+package com.te.assignment;
+
+public class TestStringFormatter {  
+public static void main(String[] args) {  
+    System.out.println(StringFormatter.reverseString("hello"));  
+    System.out.println(StringFormatter.reverseString("Welcome to this world"));      
+    }  
+}  
